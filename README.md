2dbc8b743605e29f0b1827690eaaeb6bdf6b78e9
ShubhamDebnath <gamefree89@rocketmail.com>
2021-04-04 23:04:02 +0530

    Created REST API backed, working as per requirements

---
+++ README.md
@@ -0,0 +1,159 @@
+This repo is responsible for the go code which is uploaded for lambda function
+to act as a REST API backend, all it does is, wait for a get request from AWS API gateway
+then send request to ElasticSearch with a search query
+And then send the same search results as response to whoever called this endpoint
+
+For now the search query is pretty simple
+
+```
+{
+		"size": 200,
+		"sort": { "Month": "asc", "last_update": "asc"},
+		"query": {
+		   "match_all": {}
+		}
+}
+```
+
+can easily be modified if requirements are changed
+
+Also since this API is running in AWS_PROXY mode, to enable CORS
+I just send these as response headers
+
+```
+{
+    "Access-Control-Allow-Headers": "Content-Type",
+	"Access-Control-Allow-Origin":  "*",
+	"Access-Control-Allow-Methods": "*"
+}
+```
+
+Commands used
+
+for creating the lambda function, which is hosting the rest api,
+
+```
+aws lambda create-function --function-name cupcakeAPI
+--runtime go1.x --zip-file fileb://main.zip --handler main --role arn:aws:iam::234825976347:role/service-role/892905-lambda-role
+```
+
+1. Create rest api
+
+```
+aws apigateway create-rest-api --name cupcakeRestAPI
+```
+
+```
+{
+    "id": "v9s1jkzlu3",
+    "createdDate": "2021-04-04T21:02:03+05:30",
+    "apiKeySource": "HEADER",
+    "endpointConfiguration": {
+        "types": [
+            "EDGE"
+        ]
+    },
+    "disableExecuteApiEndpoint": false
+}
+```
+
+2. Get root API resource Id
+
+```
+aws apigateway get-resources --rest-api-id v9s1jkzlu3
+```
+
+```
+{
+    "items": [
+        {
+            "id": "3o2xh8rp04",
+            "path": "/"
+        }
+    ]
+}
+```
+
+3. Create new resource under the root path
+
+```
+aws apigateway create-resource --rest-api-id v9s1jkzlu3 --parent-id 3o2xh8rp04 --path-part records
+```
+
+```
+{
+    "id": "y2c689",
+    "parentId": "3o2xh8rp04",
+    "pathPart": "records",
+    "path": "/records"
+}
+```
+
+4. Allow ANY https methods on previously created resource
+
+```
+aws apigateway put-method --rest-api-id v9s1jkzlu3 --resource-id y2c689 --http-method ANY --authorization-type NONE
+```
+
+```
+{
+    "httpMethod": "ANY",
+    "authorizationType": "NONE",
+    "apiKeyRequired": false
+}
+```
+
+5. Integrate with already created lambda function
+
+```
+aws apigateway put-integration --rest-api-id v9s1jkzlu3 --resource-id y2c689 --http-method ANY --type AWS_PROXY --integration-http-method POST --uri arn:aws:apigateway:ap-south-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ap-south-1:234825976347:function:cupcakeAPI/invocations
+```
+
+```
+{
+    "type": "AWS_PROXY",
+    "httpMethod": "POST",
+    "uri": "arn:aws:apigateway:ap-south-1:lambda:path/2015-03-31/functions/arn:aws:lambda:ap-south-1:234825976347:function:cupcakeAPI/invocations",
+    "passthroughBehavior": "WHEN_NO_MATCH",
+    "timeoutInMillis": 29000,
+    "cacheNamespace": "y2c689",
+    "cacheKeyParameters": []
+}
+```
+
+6. Give the API to access the lambda function beneath
+
+```
+aws lambda add-permission --function-name cupcakeAPI --statement-id cupcake-api-stmt-001 --action lambda:InvokeFunction --principal apigateway.amazonaws.com --source-arn arn:aws:execute-api:ap-south-1:234825976347:v9s1jkzlu3/*/*/*
+```
+
+```
+{
+    "Statement": "{\"Sid\":\"cupcake-api-stmt-001\",\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"apigateway.amazonaws.com\"},\"Action\":\"lambda:InvokeFunction\",\"Resource\":\"arn:aws:lambda:ap-south-1:234825976347:function:cupcakeAPI\",\"Condition\":{\"ArnLike\":{\"AWS:SourceArn\":\"arn:aws:execute-api:ap-south-1:234825976347:v9s1jkzlu3/*/*/*\"}}}"
+}
+```
+
+Testing the created API gateway (will fail the first time, for that need to add persions to lambda function)
+
+```
+aws apigateway test-invoke-method --rest-api-id v9s1jkzlu3 --resource-id y2c689 --http-method "GET"
+```
+
+7. If the test for api gateway is successful, deploy the api
+
+```
+aws apigateway create-deployment --rest-api-id v9s1jkzlu3 --stage-name staging
+```
+
+```
+{
+    "id": "01f7m4",
+    "createdDate": "2021-04-04T21:28:54+05:30"
+}
+```
+
+Finally access the following endpoint to see fruits of your labour
+
+```
+https://v9s1jkzlu3.execute-api.ap-south-1.amazonaws.com/staging/records
+```
---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
+)
+
+type CupcakeDoc struct {
+	Count       int    `json:"count"`
+	Date        string `json:"date"`
+	EvtType     string `json:"evtType"`
+	Last_update string `json:"last_update"`
+}
+
+var sess *session.Session
+
+// Basic information for the Amazon Elasticsearch Service domain
+var domain = "https://search-cupcake-domain-001-bdj74ottahj7ttzw3szp4e6tea.ap-south-1.es.amazonaws.com"
+var index = "cupcake-index-002"
+var endpoint = domain + "/" + index + "/" + "_search"
+var region = "ap-south-1" // e.g. us-east-1
+var service = "es"
+var signer *v4.Signer
+var client = &http.Client{}
+
+// this runs only the very first time
+// this lambda function is created
+// will be used for initial setup
+func init() {
+	sess = configureAWS()
+	signer = v4.NewSigner(sess.Config.Credentials)
+}
+
+func configureAWS() *session.Session {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("ap-south-1"),
+		Credentials: credentials.NewStaticCredentials("AKIATNLFYBIN5NZE2OXK", "WclXT3YX5rqQksVNznG5At7IL+haRkak5vD3eMri", ""),
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// adding logging handler to session
+	sess.Handlers.Send.PushFront(func(r *request.Request) {
+		// Log every request made and its payload
+		fmt.Printf("Request: %s/%s, Payload: %s",
+			r.ClientInfo.ServiceName, r.Operation, r.Params)
+	})
+
+	// fmt.Println(sess)
+
+	return sess
+}
+
+func show(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	searchReqParams := `{
+		"size": 200,
+		"sort": { "Month": "asc", "last_update": "asc"},
+		"query": {
+		   "match_all": {}
+		}
+	 }`
+	body := strings.NewReader(searchReqParams)
+
+	searchReq, err := http.NewRequest(http.MethodPost, endpoint, body)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	searchReq.Header.Add("Content-Type", "application/json")
+
+	signer.Sign(searchReq, body, service, region, time.Now())
+	resp, err := client.Do(searchReq)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var dataMap map[string]interface{}
+
+	// creating map out of response body
+	// will use selective fields out of this
+	err = json.NewDecoder(resp.Body).Decode(&dataMap)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	respSlice := make([]CupcakeDoc, 0)
+
+	// Iterate the document "hits" returned by API call
+	for _, hit := range dataMap["hits"].(map[string]interface{})["hits"].([]interface{}) {
+
+		// Parse the attributes/fields of the document
+		doc := hit.(map[string]interface{})
+		source := doc["_source"].(map[string]interface{})
+		// fmt.Println("doc _source:", reflect.TypeOf(source))
+
+		// // Get the document's _id and print it out along with _source data
+		// docID := doc["_id"]
+		// fmt.Println("docID:", docID)
+		// // fmt.Println("_source:", source, "\n")
+
+		count, _ := strconv.Atoi(source["Cupcake"].(string))
+		respSlice = append(respSlice, CupcakeDoc{Count: count, Date: source["Month"].(string), EvtType: source["evtType"].(string), Last_update: source["last_update"].(string)})
+
+	} // end of response iteration
+
+	// The APIGatewayProxyResponse.Body field needs to be a string, so
+	// we marshal the book record into JSON.
+	js, err := json.Marshal(respSlice)
+	if err != nil {
+		return serverError(err)
+	}
+
+	// Return a response with a 200 OK status and the JSON book record
+	// as the body.
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(js),
+		Headers: map[string]string{"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*"},
+	}, nil
+}
+
+// Add a helper for handling errors. This logs any error to os.Stderr
+// and returns a 500 Internal Server Error response that the AWS API
+// Gateway understands.
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+		Headers: map[string]string{"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*"},
+	}, nil
+}
+
+// Similarly add a helper for send responses relating to client errors.
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+		Headers: map[string]string{"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*"},
+	}, nil
+}
+
+func main() {
+
+	lambda.Start(show)
+
+}

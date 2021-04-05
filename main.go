c42a1c164902a59ecdc32d7052459e96fa89e745
ShubhamDebnath <gamefree89@rocketmail.com>
2021-04-05 14:59:09 +0530

    removed aws access keys from code

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func init() {
 func configureAWS() *session.Session {
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String("ap-south-1"),
-		Credentials: credentials.NewStaticCredentials("AKIATNLFYBIN5NZE2OXK", "WclXT3YX5rqQksVNznG5At7IL+haRkak5vD3eMri", ""),
+		Credentials: credentials.NewStaticCredentials("XXXXXXXXX", "XXXXXXXX", ""),
 	})
 
 	if err != nil {
